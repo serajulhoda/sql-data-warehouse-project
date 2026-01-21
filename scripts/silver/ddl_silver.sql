22707ba2990f6d994ba6011a1743e376211aa7c1
serajulhoda <serajulhoda.datascientist@gmail.com>
2026-01-21 19:08:54 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -16,9 +16,9 @@ GO
 CREATE TABLE silver.crm_cust_info (
     cst_id              INT,
     cst_key             NVARCHAR(50),
-    cst_fisrtname       NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
     cst_lastname        NVARCHAR(50),
-    cst_martial_status  NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
     cst_create_date     DATE,
     dwh_create_date DATETIME2 DEFAULT GETDATE()
